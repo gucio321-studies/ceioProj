95bfd10217bdd1de379e13d362ec23ec5f45c3db
gucio321 <gucio321@protonmail.com>
2026-04-22 09:57:17 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <TH1F.h>
+#include <TTree.h>
+#include <TFile.h>
+#include <TCanvas.h>
+#include <TLorentzVector.h>
+#include <TF1.h>
+void read_data() {
+    TFile* dataFile = new TFile("")
+}
