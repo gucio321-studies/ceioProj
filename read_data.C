68a0a5664d4f209c56842be2b61430b0d2e8db81
gucio321 <gucio321@protonmail.com>
2026-04-22 10:06:21 +0200

    read data: load branch addresses from root file

--- read_data.C
+++ read_data.C
@@ -4,6 +4,22 @@
 #include <TCanvas.h>
 #include <TLorentzVector.h>
 #include <TF1.h>
+
 void read_data() {
     TFile* dataFile = new TFile("V0_data.root");
+    TTree* dataTree = (TTree*)dataFile->Get("Ks2pipi_dd/DecayTree");
+    // read data
+    Double_t h1Px, h1Py, h1Pz, h1M,
+        h2Px, H2Py, h2Pz, h2M;
+
+    dataTree->SetBranchAddress("h1_PX", &h1Px);
+    dataTree->SetBranchAddress("h1_PY", &h1Py);
+    dataTree->SetBranchAddress("h1_PZ", &h1Pz);
+    dataTree->SetBranchAddress("h1_M", &h1M);
+
+    dataTree->SetBranchAddress("h2_PX", &h2Px);
+    dataTree->SetBranchAddress("h2_PY", &H2Py);
+    dataTree->SetBranchAddress("h2_PZ", &h2Pz);
+    dataTree->SetBranchAddress("h2_M", &h2M);
+
 }
